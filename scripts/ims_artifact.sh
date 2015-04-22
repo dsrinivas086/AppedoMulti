564898b15e84245bb584707da9dd24d20cdec537
dsrinivas086 <srinivas.scm6@gmail.com>
2015-04-22 11:47:22 +0530

    Shell Scripting files Added

---
+++ scripts/ims_artifact.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Moving Artifacts from base location to desired location
+
+cp /root/.jenkins/jobs/Appedo/workspace/ims/imstemp/*.war /tmp/artifacts
+
+echo "IMS Artifcats Moved To Safe Place"
---
+++ scripts/ups_artifact.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Moving Artifacts from base location to desired location
+
+cp /root/.jenkins/jobs/Appedo/workspace/ups/upstemp/*.war /tmp/artifacts
+
+echo "UPS Artifcats Moved To Safe Place"
+
