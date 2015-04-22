80253357fc77f9c219f6d71e220ec0dc7c68410d
dsrinivas086 <dsrao86@gmail.com>
2015-04-22 11:03:50 +0530

    Artifact Script for IMS,UPS

---
+++ scripts/ims_artifact.bat
@@ -0,0 +1,2 @@
+:: - IMS war file Moved
+xcopy "C:\Users\Srinivasarao\.jenkins\jobs\AppedoTest\workspace\ims\imstemp\*.war" "F:\Jenkins WS"
---
+++ scripts/ups_artifact.bat
@@ -0,0 +1,2 @@
+:: - UPS war file Moved to Common Location
+xcopy "C:\Users\Srinivasarao\.jenkins\jobs\AppedoTest\workspace\ups\upstemp\*.war" "F:\Jenkins WS"
