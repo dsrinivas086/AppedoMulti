b9d788855ac7442f25d1eb5a4ec72261a7589b33
dsrinivas086 <srinivas.scm6@gmail.com>
2015-04-22 12:03:18 +0530

    Shell Scripts got changed

--- scripts/ims_artifact.sh
+++ scripts/ims_artifact.sh
@@ -2,6 +2,6 @@
 
 # Moving Artifacts from base location to desired location
 
-cp /root/.jenkins/jobs/Appedo/workspace/ims/imstemp/*.war /tmp/artifacts
+scp /root/.jenkins/jobs/Appedo/workspace/ims/imstemp/*.war /tmp/artifacts
 
 echo "IMS Artifcats Moved To Safe Place"
--- scripts/ups_artifact.sh
+++ scripts/ups_artifact.sh
@@ -2,7 +2,7 @@
 
 # Moving Artifacts from base location to desired location
 
-cp /root/.jenkins/jobs/Appedo/workspace/ups/upstemp/*.war /tmp/artifacts
+scp /root/.jenkins/jobs/Appedo/workspace/ups/upstemp/*.war /tmp/artifacts
 
 echo "UPS Artifcats Moved To Safe Place"
 
